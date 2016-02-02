d578fa1b5f4007e2a9cf905d5ff829b64b1a8961
Cade Cannon <ccannon@mecum.com>
2016-02-02 09:34:20 -0600

    More service restructuring.

--- definitions/coldfusion11_service.rb
+++ definitions/coldfusion11_service.rb
@@ -1,9 +1,11 @@
 define :coldfusion11_service do
 
-  # Set up instance as a service
+ # Set up instance as a service
   service params[:name] do
-	supports :status => true, :start => true, :stop => true, :restart => true
-    action [:enable]
+    pattern "\\-Dcoldfusion\\.home=#{node['cf11']['installer']['install_folder'].gsub('/','\\\\/')}\\/#{params[:instance]} .* com\\.adobe\\.coldfusion\\.bootstrap\\.Bootstrap \\-start"
+    status_command "ps -ef | grep '\\-Dcoldfusion\\.home=#{node['cf11']['installer']['install_folder'].gsub('/','\\\\/')}\\/#{params[:instance]} .* com\\.adobe\\.coldfusion\\.bootstrap\\.Bootstrap \\-start'" if platform_family?("rhel")
+  	supports :status => true, :start => true, :stop => true, :restart => true
+    action [ :enable ]
   end
 
 end
