48bd054218ed0646f9939f28d7e0ff62d2c0ca96
Cade Cannon <ccannon@mecum.com>
2016-02-02 08:23:52 -0600

    Restructured the cf11 service definition to work around issue with starting an already started coldfusion service.

--- definitions/coldfusion11_service.rb
+++ definitions/coldfusion11_service.rb
@@ -1,11 +1,9 @@
 define :coldfusion11_service do
 
- # Set up instance as a service
+  # Set up instance as a service
   service params[:name] do
-    pattern "\\-Dcoldfusion\\.home=#{node['cf11']['installer']['install_folder'].gsub('/','\\\\/')}\\/#{params[:instance]} .* com\\.adobe\\.coldfusion\\.bootstrap\\.Bootstrap \\-start"
-    status_command "ps -ef | grep '\\-Dcoldfusion\\.home=#{node['cf11']['installer']['install_folder'].gsub('/','\\\\/')}\\/#{params[:instance]} .* com\\.adobe\\.coldfusion\\.bootstrap\\.Bootstrap \\-start'" if platform_family?("rhel")
-    supports :restart => true
-    action [ :enable, :start ]
+	supports :status => true, :start => true, :stop => true, :restart => true
+    action [:enable]
   end
 
 end
